552cb25af6d3361555316637b3c7d2adac187486
HyckaT <hyckat@gmail.com>
2025-04-15 21:47:39 +0200

    ladění + ošetření chyb

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from custom_exceptions import CardNotFound
 
 #založení objektů karet z balíčku 52 karet
 karta_1 = Card(suit="♥",value="5")
+#zkouška přepisu karty1
+karta_1 = Card(suit="♥",value="7")
 karta_2 = Card(suit="♠",value="A")
 karta_3 = Card(suit="♣",value="10")
 
@@ -15,8 +17,12 @@ karty_krupiera.add_card(karta_3)
 karty_hrace = DeckOfCards()
 karty_hrace.add_card(karta_1)
 karty_hrace.add_card(karta_2)
+
+#zkouška prázdného balíčku karet
+prazdny_balicek_karet = DeckOfCards()
+
 # tady při odebírání na špatné neexistující pozici karty vypíše chybu
-karty_hrace.remove_card(2)
+karty_hrace.remove_card(5)
 
 #výpis karet hráče vs. krupiera + počty
 print("________________________")
@@ -26,6 +32,7 @@ print("________________________")
 karty_hrace.view_cards()
 print(f"Hráč má {karty_hrace.count_of_cards()} kartu(y).")
 print("________________________")
+prazdny_balicek_karet.view_cards()
 #volání metod karet
 #karta_1.get_card()
 #karta_2.get_card()
